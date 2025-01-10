3b9f13e76d99072416c424ee38eb8192d25b0a16
Kushal-deep <kushaldeep448@gmail.com>
2025-01-10 11:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 function firstWord(s) {
   // your code here
+let index
+let index2
+for(i=0;i<s.length;i++){
+		if(s[i]!==" "){
+			index=i;
+			break;
+		}
+	}
+let ans=""
+	for(j=index;j<s.length;j++){
+		if(s[j]==" "){
+		    index2=j;
+		    break;
+		
+		}
+	}
+ans=s.slice(index,index2)
+
+	return ans
+
+	
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
